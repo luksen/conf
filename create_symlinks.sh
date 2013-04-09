c9d9aaa748d6c426ccbefb36b54dad438ded536a
Lukas Senger <lukas@fridolin.com>
2013-04-09 20:37:54 +0200

    remove unused conifgs: dwb/molokai/zsh

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -27,7 +27,6 @@ function rmln {
 
 # vim
 rmln vimrc .vimrc
-rmln molokai.vim .vim/colors/molokai.vim
 rmln wombat256mod.vim .vim/colors/wombat256mod.vim
 # i3
 rmln i3.hogship .config/i3/config hogship
@@ -36,11 +35,5 @@ rmln i3status.hogship .config/i3status/config hogship
 rmln i3status.dinghy .config/i3status/config dinghy
 # bash
 rmln bashrc .bashrc
-# zsh
-rmln zshrc .zshrc
-# dwb
-rmln dwb_settings .config/dwb/settings
-rmln dwb_keys .config/dwb/keys
-rmln dwb_searchengines .config/dwb/searchengines
 #tmux
 rmln tmux.conf .tmux.conf
